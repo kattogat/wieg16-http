e87ea2531d28e9d7a62008bb6eaccd2723701793
Maria <maria_hav@live.se>
2017-11-07 13:29:18 +0100

    Execise 4 and 6

--- functions.php
+++ functions.php
@@ -1,8 +1,31 @@
 <?php
 // "HTTP/1.0 404 Not Found"
-function status_header($code = 200) {}
+function status_header($code = 200) {
+    $messege = [
+        200 => "OK",
+        100 => "Continue",
+        301 => "Moved permanently",
+        302 => "Found",
+        403 => "Forbiden",
+        404 => "Not found",
+        500 => "Internal Server Error",
+        501 => "Not Implemented"
+    ];
+
+    header("HTTP/1.0 ".$code." ".$messege[$code]);
+}
+
+status_header(404);
 
 // [header => värde]
-function headers(array $headers = []) {}
+function headers(array $headers = []) {
+    header("HTTP/1.0 ".$code." ".$messege[$code]);
+}
+headers([
+    "Connection" => "keep-alive",
+]);
 
-function redirect($url, $code = 302) {}
+function redirect($url, $code = 302) {
+    status_header($code);
+    header('Location:'.$url);
+}
