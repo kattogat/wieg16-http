bdb212c3c261fa230f5d3c25f5d52c18662da711
Maria <maria_hav@live.se>
2017-11-07 11:08:48 +0100

    A change in the form

--- form.php
+++ form.php
@@ -1,9 +1,15 @@
 <form method="post" action="form.php">
     <label>Namn</label><br>
     <input name="name" type="text"><br>  
+    <label>mail</label><br>
+    <input type="text" name="mail"><br>
+    <label>tel</label><br>
+    <input type="number" name="tel"><br>
     <input type="submit" text="Sänd">  
 </form>
 
 <?php
-    echo $_POST["name"];
+    echo $_POST["name"] . "<br>";
+    echo $_POST["mail"] . "<br>";
+    echo $_POST["tel"] . "<br>";
 ?>
